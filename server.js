7d62df61fa0435500ba747b706ca95b26b6e64c5
smai3.shah@gmail.com <smai3.shah@gmail.com>
2017-03-04 12:17:01 -0800

    Full-Stack-Development-Mongo-DB-Node.js-React.js

---
+++ server.js
@@ -0,0 +1,3 @@
+import config, {nodeEnv,logStars} from './config';
+console.log(config, nodeEnv);
+logStars('Function');
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/_public',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
